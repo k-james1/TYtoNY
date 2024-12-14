54deb164923b762717121d9a1c6735b3ac5e6483
k-james1 <keijames.travis.5z@stu.hosei.ac.jp>
2024-12-14 16:01:40 +0900

    add
    hasta luego

---
+++ functions.js
@@ -0,0 +1,42 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const imageContainer = document.querySelector('.image-container');
+const layers = document.querySelectorAll('.image > *');
+
+gsap.set('#layer2, #layer3, #layer4', { opacity: 0});
+gsap.set('#layer1', {opacity: 1});
+
+ScrollTrigger.create({
+    trigger: imageContainer,
+    start:'top 5%',
+    end:'+=2000',
+    pin: true,
+    scrub: true,
+
+    onUpdate: (self) => {
+        if (self.progress < 0.25) {
+            gsap.to('#layer1', {opacity: 1});
+            gsap.to('#layer2,#layer3,#layer4',{opacity: 0});
+        } else if (self.progress >= 0.25 && self.progress <　0.5) {
+            gsap.to('#layer1, #layer2', {opacity: 1 });
+            gsap.to('#layer3, #layer4', {opacity: 0});
+        } else if (self.progress >= 0.5 && self.prodress < 0.75) {
+            gsap.to('#layer1, #layer2, #layer3', {opacity: 1});
+            gsap.to('#layer4', {opacity: 0});
+        } else {
+            gsap.to('#layer1, #layer2, #layer3, #layer4', {opacity: 1 });
+        }
+    },
+});
+
+gsap.from("body",{
+    scrollTrigger:{
+      trigger:"body",
+      start:"center center",
+      end:"bottom bottom",
+      scrub: true,
+    },
+    duration:0.1,
+    backgroundColor:"#1F456E",
+    ease:"none",
+  });
